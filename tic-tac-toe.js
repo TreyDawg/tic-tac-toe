197b9a0f0cb1a0760ca4ab111dc31d2d56b22f65
Trey Gewecke <trey.gewecke@yahoo.com>
2016-11-16 14:02:15 -0800

    HITHERE
    HARAMBE

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,5 +2,11 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 function circle() {
-alert(":)")
+  var svg=document.getElementById("game-board")
+  var seven=document.createElementNS(namespace, "circle")
+  seven.setAttribute("cx",50)
+  seven.setAttribute("cy",50)
+  seven.setAttribute("r",40)
+  seven.setAttribute("fill","red")
+  svg.appendChild(seven)
 }
